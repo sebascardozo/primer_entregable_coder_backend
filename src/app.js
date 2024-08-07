883bc023828b5767940f95a27a43074ed51b9267
Pablo Sebastian Cardozo <pasco_@hotmail.com>
2024-08-06 23:26:52 -0300

    app configuracion

--- src/app.js
+++ src/app.js
@@ -5,10 +5,16 @@ const app = express();
 const PUERTO = 8080;
 const productsRouter = require("./routers/products.router.js");
 const cartsRouter = require("./routers/carts.router.js");
+import { engine } from "express-handlebars";
 
 //Middleware:
 app.use(express.json());
 //Le decimos al servidor que vamos a trabajar con JSON.
+
+//Configuracion Express Handlebars
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
 
 //Rutas
 app.use("/api/products", productsRouter);
